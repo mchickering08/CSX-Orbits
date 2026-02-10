dfabd74f16185d213dfdd300d9ce2fd2da7ccbda
shepokeeffe <sokeeffe27@gcds.net>
2026-02-10 08:03:54 -0500

    editing

--- Mary and Shep Code.py
+++ Mary and Shep Code.py
@@ -2,3 +2,4 @@
 #Properties of your two masses should be consistent with the attributes of a star and a planet
 #The only constraints on your model should be that the two masses interact solely by the Law of Gravity
 #Test Shep
+#I <3 Jack Randall
